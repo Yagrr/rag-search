fd800041aff48e783f4f8cc565c678128efb5f9b
Yagrr <90156764+Yagrr@users.noreply.github.com>
2026-05-05 11:30:47 +0100

    feat(cli): add basic keyword search capabilities

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -1,7 +1,43 @@
 import argparse
+import json
+
+
+def search_keyword(kw: str, dict_movies: dict) -> list:
+    matches = []
+
+    for i in range(0, len(dict_movies["movies"])):
+        title = dict_movies["movies"][i]["title"]
+        if kw in title:
+            matches.append(title)
+
+    return matches
+
+
+def format_search_output(list_results: list, max_results: int = 0) -> str:
+    output: str = ""
+    if not list_results:
+        return "No results!"
+
+    if max_results == 0:
+        max_results = len(list_results)
+
+    for i in range(0, max_results):
+        output += f"\n{i + 1}. {list_results[i]}\n"
+
+    return output
 
 
 def main() -> None:
+
+    # ======== Print configs ========
+
+    max_results = 5
+
+    # ======== Input ========
+    with open("data/movies.json", "r") as file:
+        data_movies = json.load(file)
+
+    # ======== Parse args ========
     parser = argparse.ArgumentParser(description="Keyword Search CLI")
     subparsers = parser.add_subparsers(dest="command", help="Available commands")
 
@@ -10,11 +46,15 @@ def main() -> None:
 
     args = parser.parse_args()
 
+    # ======== Search ========
+
     match args.command:
         case "search":
             # print the search query here
             print(f"Searching for: {args.query}")
-            pass
+            results_search = search_keyword(args.query, data_movies)
+            print(format_search_output(results_search, max_results))
+
         case _:
             parser.print_help()
 
