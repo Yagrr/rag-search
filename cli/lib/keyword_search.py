d35ee203d72e93f21bba615ae67eafb2e8726e53
Yagrr <90156764+Yagrr@users.noreply.github.com>
2026-05-06 14:42:55 +0100

    refactor(search): rename variables to make more sense in search function

--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -15,11 +15,11 @@ from .utils_search import (
 # ======== Inverted Index ========
 class InvertedIndex:
     def __init__(self) -> None:
-        self.index: dict[str, set[int]] = {}
-        self.docmap: dict[int, str] = {}
+        self.index = defaultdict(set)
+        self.docmap: dict[int, dict] = {}
         self.path_index = os.path.join(PATH_CACHE, "index.pkl")
-        self.path_docmap  = os.path.join(PATH_CACHE, "docmap.pkl")
 
+        self.path_docmap = os.path.join(PATH_CACHE, "docmap.pkl")
 
     def __add_document(self, doc_id: int, text: str) -> None:
         """
@@ -51,13 +51,13 @@ class InvertedIndex:
         field_1 = "title"
         field_2 = "description"
 
-        for datum in data:
-            doc_id = datum["id"]
-            datum_field_info = f"{datum[field_1]} {datum[field_2]}"
-            self.docmap[doc_id] = datum
-            self.__add_document(doc_id, datum_field_info)
-            print(f"Added: {id}. {datum[field_1]}")
 
+        for doc in data:
+            doc_id = doc["id"]
+            doc_info = f"{doc[field_1]} {doc[field_2]}"
+            self.docmap[doc_id] = doc
+            self.__add_document(doc_id, doc_info)
+            print(f"Added: {doc_id}. {doc[field_1]}")
         return
 
     def save_cache(self) -> None:
@@ -70,16 +70,14 @@ class InvertedIndex:
         try:
             with open(self.path_index, "wb") as file:
                 pickle.dump(self.index, file)
-                print(f"Save index.pkl to disk at : {self.path_index}")
 
             with open(self.path_docmap, "wb") as file:
                 pickle.dump(self.docmap, file)
-                print(f"Save docmap.pkl to disk at : {self.path_docmap}/")
         except Exception as e:
             print(f"Error while saving: {e}")
 
-    def load_cache(self) -> None: 
-        """ 
+    def load_cache(self) -> None:
+        """
         Loads index.pkl and docmap.pkl from path_dst folder then sets the
         index and docmap of the InvertedIndex to the loaded data.
         """
@@ -101,8 +99,8 @@ class InvertedIndex:
 
 def command_search(query: str, field_to_search: str = "title", limit: int = DEFAULT_SEARCH_LIMIT) -> list[dict]:
     matches = []
-    matching_ids = []
 
+    doc_ids = []
     index = InvertedIndex()
 
     try:
@@ -113,10 +111,13 @@ def command_search(query: str, field_to_search: str = "title", limit: int = DEFA
     tokens_query: list[str] = stem_text(tokenize_text(preprocess_text(query)))
 
     for tk in tokens_query:
-        matching_ids.extend(index.get_documents(tk))
+        doc_ids.extend(index.get_documents(tk))
 
-    for id in matching_ids:
+    for id in doc_ids:
         matches.append(index.docmap[id])
+        if len(matches) >= limit:
+            break
+
     return matches
 
 
